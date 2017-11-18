41b39be4f48c2b9b65bd1592a64072d27c314f7a
Will Da Silva <will@willdasilva.xyz>
2017-11-18 14:43:59 -0500

    Change server & client code
    Various changes to server code. Server is now working (i.e. page can be opened)
    Webpage is still non-functional

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
-var http = require('http');
-var WebSocketServer = require('ws').Server; //provides web sockets
-var ecStatic = require('ecstatic');  //provides static file server service
-var url = require('url');
+const http = require('http');
+const WebSocketServer = require('ws').Server; //provides web sockets
+const ecStatic = require('ecstatic');  //provides static file server service
+const url = require('url');
 
 //static file server
-var server = http.createServer(ecStatic({root: __dirname + '/www'}));
+let server = http.createServer(ecStatic({root: __dirname + '/client'}));
 
-var wss = new WebSocketServer({server: server});
-var redditUsernamePattern = /[\w-]{20}/;
+let wss = new WebSocketServer({server: server});
+let redditUsernamePattern = /[\w-]{20}/;
 
 wss.on('connection', function(ws) { //function runs when a new connection is opened
-	console.log('New client connected');
-	
-	ws.on('message', function(data){
-		if(data){
-			var parsedData = JSON.parse(data);
-			
-			if(parsedData.type == 'profileURL'){
-				urlObj = url.parse(parsedData.data); //parse request type
-				if(urlObj.hostname === 'www.reddit.com'){
-					if(urlObj.pathname.startsWith('/u/')){
-						redditUsername = urlObj.pathname.substring(3);
-					}
-					else if(urlObj.pathname.startsWith('/user/')){
-						redditUsername = urlObj.pathname.substring(6);
-					}
-					if(redditUsernamePattern.test(redditUsername)){
-						var request = http.get('https://www.reddit.com/user/' + redditUsername + '/.json', function(response){
-							var userObj = JSON.stringify(response);
-							if(!userObj.error){
-								//Neural net processing goes here
-								//websocket return message goes here
-							}
-						});
-					}
-					else{
-						ws.send(JSON.stringify({type : error, error : 'Not a valid reddit username'}));
-					}
-				}
-			}
-			
-		}
-	});
+    console.log('New client connected');
+
+    ws.on('message', function(data){
+        if(!data) return;
+        let parsedData = JSON.parse(data);
+        if(parsedData.type == 'profileURL') {
+            urlObj = url.parse(parsedData.data); //parse request type
+            if(urlObj.hostname === 'www.reddit.com') {
+                if(urlObj.pathname.startsWith('/u/')) {
+                    redditUsername = urlObj.pathname.substring(3);
+                }
+                else if(urlObj.pathname.startsWith('/user/')) {
+                    redditUsername = urlObj.pathname.substring(6);
+                }
+                if(redditUsernamePattern.test(redditUsername)) {
+                    let request = http.get('https://www.reddit.com/user/' + redditUsername + '/.json', function(response){
+                        let userObj = JSON.stringify(response);
+                        if(!userObj.error) {
+                            //Neural net processing goes here
+                            //websocket return message goes here
+                        }
+                    });
+                }
+                else {
+                    ws.send(JSON.stringify({type : error, error : 'Not a valid reddit username'}));
+                }
+            }
+        }
+    });
 });
 
-var port = 3000;
-server.listen(port); 
-console.log('Server started on port ' + port);
+const PORT = 12601;
+server.listen(PORT, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(`Server started on port ${PORT}`);
+    }
+});
